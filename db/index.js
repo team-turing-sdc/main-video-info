3049d130c3ee0d05220b6abb2a7d7e899de193d7
Brian Kwon <bkwon94@gmail.com>
2019-03-29 16:56:42 -0400

    Added a schema for movie-info

--- db/index.js
+++ db/index.js
@@ -5,8 +5,25 @@ const db = mongoose.connection;
 db.on('connected', () => {
   console.log('fec db connected');
 })
-
+// db schema ???
+let movieInfoSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  info: {
+    description: String,
+    release: String,
+    genre: String,
+    score: Number,
+    runtime: Number,
+    image: String,
+    rating: String
+  }
+});
 // db helper functions
+let getMovieName = () => {
+  db.collection
+};
+
 
 module.exports = {
   db
