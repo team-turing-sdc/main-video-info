633c67ea8d58986c6cd0045809c5cb15ec7a6415
Brian Kwon <bkwon94@gmail.com>
2019-03-30 13:18:41 -0400

    Beginning to fill in components

---
+++ client/components/MovieInfo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MovieInfo = (props) => {
+  // look into object destructuring
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default MovieInfo;
---
+++ client/components/MoviePoster.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MoviePoster = (props) => {
+  // look into object destructuring
+  return (
+    <div>Hello</div>
+  );
+};
+
+export default MoviePoster;
