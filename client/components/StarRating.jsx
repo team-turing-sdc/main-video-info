0d0b6785d8fb5df21b665749632e1b9429a41f6d
Brian Kwon <bkwon94@gmail.com>
2019-04-02 19:44:52 -0400

    Refactor star rating to be a class component

--- client/components/StarRating.jsx
+++ client/components/StarRating.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const StarRating = ({ score }) => {
-  // look into object destructuring
-  return (
-    <span>
-      <span>&#9733;</span>
-      <span>&#9733;</span>
-      <span>&#9733;</span>
-      <span>&#9733;</span>
-      <span>&#9733;</span>
-    </span>
 
-  );
-};
+
+class StarRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <span>
+        <span>&#9733;</span>
+        <span>&#9733;</span>
+        <span>&#9733;</span>
+        <span>&#9733;</span>
+        <span>&#9733;</span>
+      </span>
+
+    );
+  }
+
+}
 
 export default StarRating;
