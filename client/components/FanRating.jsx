5a4ffdc4ccf65d93caa4a7be59165fb876a53db0
Brian Kwon <bkwon94@gmail.com>
2019-04-03 00:09:17 -0400

    Add three style components for icon, percentage and branding

--- client/components/FanRating.jsx
+++ client/components/FanRating.jsx
@@ -16,6 +16,31 @@ const ScoreContainer = styled.div`
   margin-top: 1em;
 `;
 
+const Percentage = styled.span`
+  grid-column: 3 / 5;
+  color: white;
+  font-family: ProximaNovaW01-Bold;
+  font-weight: bold;
+  padding-top: 1.5vh;
+  padding-left: 0.1vw;
+`;
+
+const Image = styled.span`
+  grid-column: 1 / 3;
+  margin-left: auto;
+`;
+
+const Branding = styled.span`
+  color: white;
+  font-family: "Times New Roman", serif;
+  font-style: italic;
+  grid-column: 2 / 5;
+  grid-row: 2 / 3;
+  margin-right: 2.25vw;
+  font-size 12px;
+  text-align: center;
+`;
+
 
 const FanRating = (props) => {
   let percentageScore = Math.floor(Math.random() * 100);
@@ -24,8 +49,11 @@ const FanRating = (props) => {
       <div>
         <FanRatingWrapper>{Math.floor(Math.random() * 50000)} Fan Ratings</FanRatingWrapper>
         <ScoreContainer>
-          <img src="https://img.icons8.com/color/48/000000/tomato.png"/>
-          <span>{percentageScore}%</span>
+          <Image>
+            <img src="https://img.icons8.com/color/48/000000/tomato.png"/>
+          </Image>
+          <Percentage>{percentageScore}%</Percentage>
+          <Branding>Rotting Tomatoes&trade;</Branding>
         </ScoreContainer>
 
       </div>
@@ -35,8 +63,11 @@ const FanRating = (props) => {
       <div>
         <FanRatingWrapper>{Math.floor(Math.random() * 50000)} Fan Ratings</FanRatingWrapper>
         <ScoreContainer>
-          <img src="https://img.icons8.com/color/48/000000/rotten-tomatoes.png"/>
-          <span>{percentageScore}%</span>
+          <Image>
+            <img src="https://img.icons8.com/color/48/000000/rotten-tomatoes.png"/>
+          </Image>
+          <Percentage>{percentageScore}%</Percentage>
+          <Branding>Rotting Tomatoes&trade;</Branding>
         </ScoreContainer>
 
       </div>
