5f17c8c561444b0f32c95808752ce0f0d6bf12e3
Brian Kwon <bkwon94@gmail.com>
2019-04-03 23:12:48 -0400

    Add styling to Title styled component

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -5,17 +5,22 @@ const OptionsContainer = styled.div`
   grid-column: 1 / 3;
   grid-row: 2 / 3;
   border: 1px solid #5c5c5c;
-  text-align: left;
   font-family: Proxima Nova;
+  overflow: hidden;
 `;
 const Title = styled.div`
   display: inline-block;
   background-color: #5c5c5c;
-  font-size: 14px;
+  font-family: Proxima Nova;
+  font-size: 13px;
   color: #c4c4c4;
+  height: 100%;
+  width: 15%;
+  overflow: hidden;
   text-align: center;
-  height: 95%;
-  width: 20%;
+  padding: 1.75vh 0.75vw 1vh 0.75vw;
+  vertical-align: top;
+  transform: scale(1 , 1.45);
 `;
 const Options = (props) => {
   // look into object destructuring
