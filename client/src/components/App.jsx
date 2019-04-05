be3b1aaaea7b20a4c39344ce80c974b772b534a7
Brian Kwon <bkwon94@gmail.com>
2019-04-04 23:04:14 -0400

    Remove key

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -103,7 +103,7 @@ class App extends React.Component {
           headers: {
             'Content-Type': 'application/json',
             'client': 'PERS_25',
-            'x-api-key': 'mgISY6IvC42LS6RSjGp8y4OIzdXOxKT84tjo92Vu',
+            'x-api-key': '',
             'authorization': 'Basic UEVSU18yNTpQQVlobHhhT0RjcE4=',
             'api-version': 'v200',
             'territory': 'US',
