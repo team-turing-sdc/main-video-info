6c7cec59ff13adbc62f6cfc2d188346cf22874d9
Brian Kwon <bkwon94@gmail.com>
2019-04-05 17:49:05 -0400

    Style theater name. Add hover effect and smooth transition

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TheaterName = styled.a`
-  
+  text-decoration: none;
+  text-transform: uppercase;
+  font-family: Alternate Gothic No1 D;
+  font-size: 2em;
+  font-weight: 700;
+  color: #3d7de5;
+  letter-spacing: 1px;
+  :hover {
+    color: #2d77ef;
+    transition: .3s linear;
+  }
 `;
 const ShowDetails = (props) => {
   // look into object destructuring
   return (
     <div>
-      <a href="#">{props.cinema.cinema_name}</a>
+      <TheaterName href="#">{props.cinema.cinema_name}</TheaterName>
     </div>
   );
 };
