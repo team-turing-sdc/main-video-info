2f40ebe40d6dc9bd3bd714732021565f11b4be36
Brian Kwon <bkwon94@gmail.com>
2019-04-05 18:44:02 -0400

    Add functionality to get current date in proper format

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -14,10 +14,24 @@ const TheaterName = styled.a`
     transition: .3s linear;
   }
 `;
+
+const Subheading = styled.div`
+  color: #ccc;
+  font-family: Proxima Nova, Helvetica;
+  font-size: 1em;
+
+`;
 const ShowDetails = (props) => {
+  let currentDate = new Date();
+  let month = currentDate.toLocaleString('en-us', { month: 'long'});
+  let day = currentDate.getUTCDate();
+  let dayOfWeek = currentDate.toLocaleString('en-us', { weekday: 'long' });
+  let year = currentDate.getUTCFullYear();
   return (
     <div>
       <TheaterName href="#">{props.cinema.cinema_name}</TheaterName>
+      <Subheading>Movie Times for {`${dayOfWeek}, ${month} ${day}, ${year}`}</Subheading>
+      {/* showtime buttons */}
       {
         props.cinema.showings.Standard.times.map((showing, i) => {
           return <Showtimes key={i} showing={showing}/>
