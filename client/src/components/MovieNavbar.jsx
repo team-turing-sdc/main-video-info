56526adcbcf010105ac55357ea9cb6249ca98c84
Brian Kwon <bkwon94@gmail.com>
2019-04-08 14:10:32 -0400

    Finish essential styling to movie nav bar. Add hover effects

--- client/src/components/MovieNavbar.jsx
+++ client/src/components/MovieNavbar.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const NavWrapper = styled.div`
   display: grid;
-  grid-template-columns: 3fr 1fr;
+  grid-template-columns: 3.5fr 1fr;
   grid-template-rows: 0.75fr 0.45fr;
   background: rgba(76, 76, 76, 0.5);
   font-family: Alternate Gothic No1 D;
@@ -13,36 +13,61 @@ const MovieTitle = styled.h1`
   color: white;
   text-transform: uppercase;
   font-size: 3.5em;
+  margin: 0;
+  padding-bottom: 0.5vh;
 `;
 const Nav = styled.ul`
   grid-column: 1 / 3;
   grid-row: 2 / 3;
   padding: 0;
   list-style: none;
-  margin: 0;
+  margin: 0.75vh 0 0 0;
+  color: white;
+  :hover {
+    color: #ccc;
+    transition: all .2s ease-in-out;
+  }
 `;
 
 const NavItem = styled.li`
   float: left;
-  color: white;
   margin-right: 3.5vw;
   font-size: 1.1em;
   font-family: ProximaNovaW01-Bold, Helvetica;
   text-transform: uppercase;
+  :hover {
+    color: white;
+    transition: all .2s ease-in-out;
+  }
+`;
+
+const HeartIcon = styled.h1`
+  font-family: Proxima Nova;
+  font-size: 0.75em;
+  margin: 0;
+  padding-bottom: 0.5vh;
+  color: white;
+  display: inline-block;
+`;
+
+const ArrowIcon = styled.span`
+  color: #f15500;
 `;
 
 const MovieNavbar = (props) => {
 
   return (
     <NavWrapper>
-      <MovieTitle>{props.movie.info.name} ({props.movie.info.releaseYear})  <i class="far fa-heart"></i></MovieTitle>
+      <MovieTitle>{props.movie.info.name} ({props.movie.info.releaseYear})  <HeartIcon> <i class="far fa-heart"></i></HeartIcon></MovieTitle>
       <Nav>
         <NavItem>Overview</NavItem>
         <NavItem>Movie Times + Tickets</NavItem>
         <NavItem>Synopsis</NavItem>
         <NavItem>Movie Reviews</NavItem>
         <NavItem>Trailers</NavItem>
-        <NavItem>More</NavItem>
+        <NavItem>
+          More <ArrowIcon className="fas fa-caret-down"></ArrowIcon>
+        </NavItem>
       </Nav>
     </NavWrapper>
   );
