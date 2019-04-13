6a8016837c34ab0c653094af00935409633c61da
Brian Kwon <bkwon94@gmail.com>
2019-04-13 01:19:26 -0400

    Allow user to click venue name for directions and hover for instructions

--- client/src/components/ShowDetails.jsx
+++ client/src/components/ShowDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
-import Showtimes from './Showtimes.jsx'
+import Showtimes from './Showtimes.jsx';
+import ReactToolTip from 'react-tooltip';
 
 
 const TheaterName = window.styled.a`
@@ -65,6 +66,14 @@ const PopupContent = window.styled.p`
   padding: 3px;
   font-size: 12px;
 `
+const PopupLink = window.styled.a`
+  color: #ccc;
+  :hover {
+    color: white;
+    transition: .3s linear;
+  }
+  text-decoration: none;
+`;
 
 const ShowDetails = (props) => {
   // generate date in proper format
@@ -73,40 +82,53 @@ const ShowDetails = (props) => {
   let day = currentDate.getUTCDate();
   let dayOfWeek = currentDate.toLocaleString('en-us', { weekday: 'long' });
   let year = currentDate.getUTCFullYear();
+
+  let link = `https://www.google.com/maps/dir/?api=1&destination=${props.cinema.cinema_name}+Phildelphia+PA&travelmode=car`
   return (
     <div>
-      <TheaterName href="#">{props.cinema.cinema_name}</TheaterName>
+      <TheaterName data-tip="Click for directions!" href={link} target="_blank">{props.cinema.cinema_name}</TheaterName>
+      <ReactToolTip place="top" type="light" effect="float"/>
       <Subheading>Movie Times for {`${dayOfWeek}, ${month} ${day}, ${year}`}</Subheading>
       {/* Seating Amenities */}
       {/* hard coded since info is not available in API used */}
       <SeatingAmenitiesList>
 
         <AmenitiesItem>
-          <Popup trigger={<p> Closed caption  &#8226;</p>} position="right center">
-            <PopupHeader>CLOSED CAPTION</PopupHeader>
-            <PopupContent>Closed Captioning devices display a movie's dialogue and sound effects as text; captions are not shown on the main screen.  Devices available by request.</PopupContent>
-          </Popup>
+          <PopupLink href="#">
+            <Popup trigger={<p> Closed caption  &#8226;</p>} position="right center">
+              <PopupHeader>CLOSED CAPTION</PopupHeader>
+              <PopupContent>Closed Captioning devices display a movie's dialogue and sound effects as text; captions are not shown on the main screen.  Devices available by request.</PopupContent>
+            </Popup>
+          </PopupLink>
         </AmenitiesItem>
 
         <AmenitiesItem>
-          <Popup trigger={<p> Accessibility devices available  &#8226;</p>} position="right center">
-            <PopupHeader>Accessibility devices available</PopupHeader>
-            <PopupContent>Descriptive Video devices provide audio descriptions of the movie to accommodate the needs of visually impaired guests. Devices available by request.</PopupContent>
-          </Popup>
+          <PopupLink>
+            <Popup trigger={<p> Accessibility devices available  &#8226;</p>} position="right center">
+              <PopupHeader>Accessibility devices available</PopupHeader>
+              <PopupContent>Descriptive Video devices provide audio descriptions of the movie to accommodate the needs of visually impaired guests. Devices available by request.</PopupContent>
+            </Popup>
+          </PopupLink>
         </AmenitiesItem>
 
         <AmenitiesItem>
-          <Popup trigger={<p> Recliner Seats  &#8226;</p>} position="right center">
+          <PopupLink>
+            <Popup trigger={<p> Recliner Seats  &#8226;</p>} position="right center">
               <PopupHeader>Recliner Seats</PopupHeader>
               <PopupContent>Kick back in an extra-comfy recliner and enjoy the show.</PopupContent>
-          </Popup>
+            </Popup>
+          </PopupLink>
         </AmenitiesItem>
+
         <AmenitiesItem>
-          <Popup trigger={<p> <i className="fas fa-chair"></i> Reserved seating</p>} position="right center">
-             <PopupHeader>Reserved seating</PopupHeader>
-             <PopupContent>During checkout you will be able to reserve specific seats or be assigned seats in a premier seating area, depending on the theater.</PopupContent>
-          </Popup>
+          <PopupLink>
+            <Popup trigger={<p> <i className="fas fa-chair"></i> Reserved seating</p>} position="right center">
+               <PopupHeader>Reserved seating</PopupHeader>
+               <PopupContent>During checkout you will be able to reserve specific seats or be assigned seats in a premier seating area, depending on the theater.</PopupContent>
+            </Popup>
+          </PopupLink>
         </AmenitiesItem>
+
       </SeatingAmenitiesList>
 
       {/* showtime buttons */}
