10b03aa964fff746fd8c30cc6be2ab17a0b106ab
NamVo5009 <envytheroy@gmail.com>
2019-04-22 13:58:06 -0400

    added data generator

--- db/postgres.js
+++ db/postgres.js
@@ -1,6 +1,23 @@
 var pgp = require('pg-promise')(/*options*/)
 var db = pgp('postgres://me:password@localhost:5432/mydb')
 
+const seedDb = () => {
+  var values = {
+    delimiter: ',',
+    path: '/mnt/c/Users/Mich/Documents/GitHub/main-video-info/db/titleInfo.csv'
+  };
+  db.none('CREATE TABLE movie(name, description ,genre ,score ,runtime , image, rating ,releaseMonth ,releaseDay , releaseYear')
+    .then(() =>{
+      db.none('COPY (SELECT * FROM titleInfo) TO ${path} WITH CSV DELIMITER ${delimiter}', values)
+        .then(() => {
+          console.log('SEEDED')
+        })
+      })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
 const postNewMovie = (movieInfo, callback) =>{
   const {name,description,releaseng,genre,score,runtime,image,rating} = movieInfo;
   db.none('INSERT INTO movies(name,description,releaseng,genre,score,runtime,image,rating) VALUES($1, $2, $3, $4, $5, $6, $7, $8)',[name,description,releaseng,genre,score,runtime,image,rating])
@@ -11,8 +28,9 @@ const postNewMovie = (movieInfo, callback) =>{
       console.log(error);
     })
 }
-
+seedDb();
 module.exports ={
+  seedDb,
   postNewMovie
 }
 
